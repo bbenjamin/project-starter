d731ef34836ed7dae70bc0797e696c9f38060d34
Ben Mullins <benm@umich.edu>
2026-08-27 22:18:16 -0400

    ugh

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello! I'm Ben</Text>
+      <Text>Hello! I'm Ben Again</Text>
       <StatusBar style="auto" />
     </View>
   );
